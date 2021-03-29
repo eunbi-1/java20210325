363ff9502bea69afacf3e05da9ad19c0598cfd38
eunbi-1 <pjo579@gmail.com>
2021-03-29 11:45:29 +0900

    .

---
+++ src/p03/lecture/p2binary/artihmetic/A02Overflow.java
@@ -0,0 +1,19 @@
+package p03.lecture.p2binary.artihmetic;
+
+public class A02Overflow {
+	public static void main(String[] args) {
+		int a = 20_0000_0000;
+		int b = 20_0000_0000;
+		
+		int c = a + b;
+		
+		System.out.println(c);
+		
+		long x = 20_0000_0000;
+		long y = 20_0000_0000;
+		
+		long z = x + y;
+		System.out.println(z);
+//		데이터 타입의 크기 차이 MAX
+	}
+}
