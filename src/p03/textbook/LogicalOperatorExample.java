380ff71985e86da5856fd8def6adee8864d59a8f
eunbi-1 <pjo579@gmail.com>
2021-03-29 16:40:59 +0900

    .

--- src/p03/textbook/LogicalOperatorExample.java
+++ src/p03/textbook/LogicalOperatorExample.java
@@ -10,7 +10,7 @@ public class LogicalOperatorExample {
 		if( (charCode>=97) & (charCode<=122)) {
 			System.out.println("소문자 이군요");
 		}	
-		if (!(charCode<48) && !(charCode>57)) {
+	 	if (!(charCode<48) && !(charCode>57)) {
 			System.out.println("0~9 숫자 이군요");
 		}
 		int value = 6;
