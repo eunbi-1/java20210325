6d28bcc5410e1ba482c5fdb60e8d9b2fd7e3c763
eunbi-1 <pjo579@gmail.com>
2021-04-14 15:30:10 +0900

    .

---
+++ src/p08/textbook/s080503/Car.java
@@ -0,0 +1,16 @@
+package p08.textbook.s080503;
+
+public class Car {
+	Tire[] tires = {
+		new HankookTire(),
+		new HankookTire(),
+		new HankookTire(),
+		new HankookTire()
+	};
+	
+	void run() {
+		for(Tire tire : tires) {
+			tire.roll();
+		}
+	}
+}
